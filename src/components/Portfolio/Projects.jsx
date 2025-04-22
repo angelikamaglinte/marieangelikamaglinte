7727983f247b5f97b74b3562f9a3853f125f7ef7
Angelika Maglinte <53634332+angelikamaglinte@users.noreply.github.com>
2025-04-21 23:51:56 -0600

    Angel: Updated project URL links.

--- src/components/Portfolio/Projects.jsx
+++ src/components/Portfolio/Projects.jsx
@@ -4,42 +4,42 @@ const projects = [
         title: "GLUU",
         tags: ["UI/UX Design", "Full-Stack Development", "Web Development", "Web Design"],
         image: "gluu_mockup",
-        url: "gluu-repair.vercel.app"
+        url: "https://gluu-repair.vercel.app"
     },
     {
         year: 2025,
         title: "PLANTIFY APP",
         tags: ["UI/UX Design", "Mobile App Design", "App Development", "Branding"],
         image: "plantify_mockup",
-        url: "gluu-repair.vercel.app"
+        url: "https://gluu-repair.vercel.app"
     },
     {
         year: 2024,
         title: "AM DESIGNS",
         tags: ["UI/UX Design", "Full-Stack Development", "Web Development", "Web Design"],
         image: "amd_mockup",
-        url: "amdesignsstd.vercel.app"
+        url: "https://amdesignsstd.vercel.app"
     },
     {
         year: 2024,
         title: "NOSH & NOOK",
         tags: ["UI/UX Design", "Full-Stack Development", "Web Development", "Web Design"],
         image: "nosh_mockup",
-        url: "gluu-repair.vercel.app"
+        url: "https://gluu-repair.vercel.app"
     },
     {
         year: 2024,
         title: "PAGEPILOT",
         tags: ["UI/UX Design", "Full-Stack Development", "Web Development", "Web Design"],
         image: "page_pilot",
-        url: "gluu-repair.vercel.app"
+        url: "https://gluu-repair.vercel.app"
     },
     {
         year: 2024,
         title: "FLEX WORKSPACE",
         tags: ["UI/UX Design", "Full-Stack Development", "Web Development", "Web Design"],
         image: "flex_workspace",
-        url: "gluu-repair.vercel.app"
+        url: "https://gluu-repair.vercel.app"
     },
 ];
 
