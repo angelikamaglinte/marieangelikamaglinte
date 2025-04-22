f7db05abd2d7e02bad525efb8f38025bd2bdac62
Angelika Maglinte <53634332+angelikamaglinte@users.noreply.github.com>
2025-04-21 23:47:28 -0600

    Angel: Updated url for am designs project.

--- src/components/Portfolio/Projects.jsx
+++ src/components/Portfolio/Projects.jsx
@@ -4,42 +4,42 @@ const projects = [
         title: "GLUU",
         tags: ["UI/UX Design", "Full-Stack Development", "Web Development", "Web Design"],
         image: "gluu_mockup",
-        url: "https://google.ca"
+        url: "gluu-repair.vercel.app"
     },
     {
         year: 2025,
         title: "PLANTIFY APP",
         tags: ["UI/UX Design", "Mobile App Design", "App Development", "Branding"],
         image: "plantify_mockup",
-        url: "https://google.ca"
+        url: "gluu-repair.vercel.app"
     },
     {
         year: 2024,
-        title: "AMD STUDIO",
+        title: "AM DESIGNS",
         tags: ["UI/UX Design", "Full-Stack Development", "Web Development", "Web Design"],
         image: "amd_mockup",
-        url: "https://google.ca"
+        url: "amdesignsstd.vercel.app"
     },
     {
         year: 2024,
         title: "NOSH & NOOK",
         tags: ["UI/UX Design", "Full-Stack Development", "Web Development", "Web Design"],
         image: "nosh_mockup",
-        url: "https://google.ca"
+        url: "gluu-repair.vercel.app"
     },
     {
         year: 2024,
         title: "PAGEPILOT",
         tags: ["UI/UX Design", "Full-Stack Development", "Web Development", "Web Design"],
         image: "page_pilot",
-        url: "https://google.ca"
+        url: "gluu-repair.vercel.app"
     },
     {
         year: 2024,
         title: "FLEX WORKSPACE",
         tags: ["UI/UX Design", "Full-Stack Development", "Web Development", "Web Design"],
         image: "flex_workspace",
-        url: "https://google.ca"
+        url: "gluu-repair.vercel.app"
     },
 ];
 
